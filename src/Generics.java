0720fce9acbb920b2fe61b4dc81e126409a1dd13
Juanfelipepast2 <72947090+Juanfelipepast2@users.noreply.github.com>
2021-05-05 10:32:51 -0500

    Update Generics.java
    holu uwu

--- src/Generics.java
+++ src/Generics.java
@@ -1,16 +1,7 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class Generics<T> { //se usa la letra T en modo de identificación de la palabra Type para especificar un tipo de dato que podrá ser agregado
     private T atributo ;
     
